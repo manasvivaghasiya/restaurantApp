1329b1f13346a07f114ed02b378c1fa1d8712f56
manasvivaghasiya <96326072+manasvivaghasiya@users.noreply.github.com>
2022-01-10 17:25:19 +0530

    login

--- restaurant/src/app/forgot-pass/forgot-pass.component.ts
+++ restaurant/src/app/forgot-pass/forgot-pass.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-pass',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPassComponent implements OnInit {
 
-  constructor() { }
+  user: FormGroup
 
+  constructor(private formBuilder: FormBuilder) { 
+    this.user = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+   })
+  }
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   }
 
-}
+
+
+
--- restaurant/src/app/registration/registration.component.ts
+++ restaurant/src/app/registration/registration.component.ts
@@ -1,22 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-// import {  NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { WelcomeComponent } from '../welcome/welcome.component';
 
+// @Component({
+//   selector: 'ngbd-modal-content',
+//   templateUrl:  
+
+// })
+// export class NgbdModalContent {
+// @Input() name: any;
+
+// constructor(public activeModal: NgbActiveModal) {}
+// }
+
+
+
+// ---
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent  {
-  
-  constructor() {}
+export class RegistrationComponent {
+  user: FormGroup;
 
-//   open() {
-//     const modalRef = this.modalService.open(NgbdModalContent);
-//     modalRef.componentInstance.name = 'welcome';
-//   }
-// }
+  constructor(private modalService: NgbModal, private formBuilder: FormBuilder) {
+    this.user = this.formBuilder.group({
+      fristName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      contectNo: ['', [Validators.required]],
+    })
+  }
+
+  open() {
+    const modalRef = this.modalService.open(WelcomeComponent);
+    modalRef.componentInstance.name = 'welcome';
+  }
+}
 // function NgbdModalContent(NgbdModalContent: any) {
 //   throw new Error('Function not implemented.');
 // }
 
-}
