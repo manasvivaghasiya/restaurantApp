1714013872ad5f0c838d28ac56ef94574f580098
manasvivaghasiya <96326072+manasvivaghasiya@users.noreply.github.com>
2022-01-24 10:00:57 +0530

    login

--- restaurant/src/app/crud/crud.component.ts
+++ restaurant/src/app/crud/crud.component.ts
@@ -12,6 +12,7 @@ export class CrudComponent implements OnInit {
   user!: FormGroup;
   http: any;
   userData: any;
+  editUserInfo: any;
 
 
   constructor(private formBuilder: FormBuilder) {
@@ -38,6 +39,31 @@ deleteUser(id:number){
     }else{
       alert(res.message)
     }
+  })
+}
+updateUser(){
+  this.http.update(`${environment.apiEndPoint}/api/user/update`,{
+    ...this.editUserInfo,
+    id:this.editUserInfo.id,
+    ...this.user.value
+  }).subscribe((res:any) =>{
+    if(res.isSuccess){
+      this.editUserInfo = null
+      alert('data updated successfully')
+      this.user.reset()
+      this.getUser()
+    }else{
+      alert(res.message)
+    }
+  })
+}
+
+editUser(user:any){
+  this.editUserInfo = user
+  this.user.patchValue({
+    email:user.email,
+    fullName:user.fullName,
+    mobileName:user.mobileName
   })
 }
 
